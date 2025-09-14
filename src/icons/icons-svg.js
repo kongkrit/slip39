019d91ebd2b53ac5969572e907bccc5af5d862c1
kongkrit <45794368+kongkrit@users.noreply.github.com>
2025-09-15 01:49:47 +0700

    add chevrons icons

--- src/icons/icons-svg.js
+++ src/icons/icons-svg.js
@@ -1,3 +1,12 @@
+// icons are mostly from feathericons.com
+// icon JS-name, feathericons name, small description
+//
+// eyeOpen       eye                open eye. reveal passwords
+// eyeClosed     eye-off            open eye with diagonal line. hide password.
+// copy          copy               copy info to clipboard
+// chevronsLeft  chevrons-left      double chevrons left. decrement value.
+// chevronsRight chevrons-right     double chevrons right. increment value.
+
 (function (root){
   root.icons = root.icons || {};
   
@@ -49,6 +58,40 @@
   <g fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 	<rect x="9" y="9" width="13" height="13" rx="2" ry="2"/>
 	<path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>
+  </g>
+</svg>
+
+`;
+
+  // --- double chevrons left. decrease value ---
+  root.icons.chevronsLeft = `
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <style>
+    svg { color:#0b0b0c; }
+    @media (prefers-color-scheme: dark) { svg { color:#e8e8ea; } }
+    @media (forced-colors: active) { svg { color: CanvasText; } }
+  </style>
+  <g fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+    <polyline points="11 17 6 12 11 7"/>
+	<polyline points="18 17 13 12 18 7"/>
+  </g>
+</svg>
+
+`;
+
+  // --- double chevrons right. increase value ---
+  root.icons.chevronsRight = `
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <style>
+    svg { color:#0b0b0c; }
+    @media (prefers-color-scheme: dark) { svg { color:#e8e8ea; } }
+    @media (forced-colors: active) { svg { color: CanvasText; } }
+  </style>
+  <g fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+    <polyline points="13 17 18 12 13 7"/>
+	<polyline points="6 17 11 12 6 7"/>
   </g>
 </svg>
 
